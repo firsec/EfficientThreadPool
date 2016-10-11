491757b9ef7addad469e668b26216e4b2d5b8e03
haoyu <toget@qq.com>
2016-10-11 13:05:17 +0800

    add file structure

---
+++ JoobPool.hpp
@@ -0,0 +1,12 @@
+#ifndef __JOBPOOL__
+#define __JOBPOOL__
+#include<unordered_map>
+
+namespace JobPool {
+  class Scheduler {
+  }
+  class JobPool {
+  }
+}
+
+#endif
---
+++ ThreadPool.hpp
@@ -0,0 +1,13 @@
+#ifndef __THREADPOOL__
+#define __THREADPOOL__
+#include<unordered_map>
+#include<thread>
+
+namespace ThreadPool {
+  class Scheduler {
+  }
+  class JobPool {
+  }
+}
+
+#endif
